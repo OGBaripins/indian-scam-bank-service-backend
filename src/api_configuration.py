9cae0c9140c4f2e67407842ed760d85d10a3b3d1
ZKaspars <90309385+ZKaspars@users.noreply.github.com>
2022-09-16 22:24:49 +0300

    yesz

--- src/api_configuration.py
+++ src/api_configuration.py
@@ -1,18 +1,31 @@
 from datetime import datetime
+
 import simplejson as json
+from decimal import Decimal
+
 from flask import Flask
 from flask_restful import Api, Resource
-import etc.helpers as helpers
+
 import db_queries
 
 app = Flask(__name__)
 api = Api(app)
 
 
+def convert_to_json(data):
+    data = json.dumps(data, use_decimal=True, default=myconverter)
+    return json.loads(str(data).replace("'", '"'))
+
+
+def myconverter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
+
+
 class Accounts(Resource):
 
     def get(self):
-        return helpers.convert_to_json(db_queries.get_accounts())
+        return convert_to_json(db_queries.get_accounts())
 
     def post(self):
         pass
@@ -24,7 +37,7 @@ class Accounts(Resource):
 class Credentials(Resource):
 
     def get(self):
-        return helpers.convert_to_json(db_queries.get_credentials())
+        return convert_to_json(db_queries.get_credentials())
 
     def post(self):
         pass
@@ -36,7 +49,43 @@ class Credentials(Resource):
 class Transactions(Resource):
 
     def get(self):
-        return helpers.convert_to_json(db_queries.get_transactions())
+        return convert_to_json(db_queries.get_transactions())
+
+    def post(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class TransactionsById(Resource):
+
+    def get(self, trid="newId"):
+        return {convert_to_json(db_queries.get_transactionsById(var=trid))}
+
+    def post(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class TransactionsByReceiverName(Resource):
+
+    def get(self):
+        return convert_to_json(db_queries.get_transactionsByReceiverName())
+
+    def post(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class TransactionsByAccNumber(Resource):
+
+    def get(self):
+        return convert_to_json(db_queries.get_transactionsByAccNumber())
 
     def post(self):
         pass
@@ -47,6 +96,9 @@ class Transactions(Resource):
 
 api.add_resource(Credentials, "/credentials")
 api.add_resource(Transactions, "/transactions")
+api.add_resource(TransactionsById, "/transactions/transactionsById/<string:trId>")
+api.add_resource(TransactionsByReceiverName, "/transactions/transactionsByReceiverName")
+api.add_resource(TransactionsByAccNumber, "/transactions/transactionsByAccNumber")
 api.add_resource(Accounts, "/accounts")
 
 if __name__ == "__main__":
--- src/db_queries.py
+++ src/db_queries.py
@@ -1,22 +1,17 @@
 import mysql.connector
-import etc.helpers as helpers
 
 
 def con():
-    conf = helpers.create_conf("../etc/conf.yaml", "DATABASE_CON")
-
-    print(conf)
-
     try:
         mydb = mysql.connector.connect(
-            host=conf.get("HOST"),
-            port=conf.get("PORT"),
-            user=conf.get("USER"),
-            password=conf.get("PASSWORD"),
-            database=conf.get("DATABASE")
+            host="sql11.freesqldatabase.com",
+            port="3306",
+            user="sql11520035",
+            password="WjRaAgGLiL",
+            database="sql11520035"
         )
-    except mysql.connector.Error as err:
-        print(f"Connection to database was unsuccessful\nErr: {err}")
+    except:
+        print("Connection to database was unsuccessful")
     return mydb
 
 
@@ -65,7 +60,58 @@ def get_transactions():
         cur.close()
         mydb.close()
     except mysql.connector.Error as err:
-        print("Couldn't retrieve information for Credentials table\n", err)
+        print("Couldn't retrieve information for Transactions table\n", err)
+
+    finally:
+        return data
+
+
+def get_transactionsById(var):
+    mydb = con()
+    cur = mydb.cursor(buffered=True, dictionary=True)
+    sql_post = "SELECT account_id, amount, reciver_account_number, reciver_name, transaction_date, transaction_id " \
+               "FROM transactions WHERE transaction_id like %s"
+    try:
+        cur.execute(sql_post, tuple(var))
+        data = cur.fetchall()
+        cur.close()
+        mydb.close()
+    except mysql.connector.Error as err:
+        print("Couldn't retrieve information for Transactions table\n", err)
+
+    finally:
+        return data
+
+
+def get_transactionsByReceiverName(var):
+    mydb = con()
+    cur = mydb.cursor(buffered=True, dictionary=True)
+    sql_post = "SELECT account_id, amount, reciver_account_number, reciver_name, transaction_date, transaction_id " \
+               "FROM transactions WHERE reciver_name like %s"
+    try:
+        cur.execute(sql_post, tuple(var))
+        data = cur.fetchall()
+        cur.close()
+        mydb.close()
+    except mysql.connector.Error as err:
+        print("Couldn't retrieve information for Transactions table\n", err)
+
+    finally:
+        return data
+
+
+def get_transactionsByAccNumber(var):
+    mydb = con()
+    cur = mydb.cursor(buffered=True, dictionary=True)
+    sql_post = "SELECT account_id, amount, reciver_account_number, reciver_name, transaction_date, transaction_id " \
+               "FROM transactions WHERE reciver_account_number like %s"
+    try:
+        cur.execute(sql_post, tuple(var))
+        data = cur.fetchall()
+        cur.close()
+        mydb.close()
+    except mysql.connector.Error as err:
+        print("Couldn't retrieve information for Transactions table\n", err)
 
     finally:
         return data
